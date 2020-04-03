50eaa823976dd102a50ca443f2431c2f744d2542
Bernhard Danecker <bernhard.danecker@gmail.com>
2020-04-03 16:42:03 +0200

    level4 solution

--- src/Main.kt
+++ src/Main.kt
@@ -7,7 +7,7 @@ import kotlin.math.sin
 
 fun main() {
 
-    val level = "level3"
+    val level = "level4"
 
     File("input/$level").listFiles { _, filename -> filename.endsWith(".in") }!!.sortedBy { it.name }.forEach {
         val result = processFile(it)
@@ -26,15 +26,43 @@ fun main() {
     }
 }
 
+fun scanner(text: String) = Scanner(text).apply {
+    useDelimiter(Pattern.compile("[\n, ]"))
+    useLocale(Locale.ROOT)
+}
+
 data class Flight(
-    val timestamp: Long,
-    val lat: Float,
-    val lon: Float,
-    val alt: Float,
-    val start: String,
+    val origin: String,
     val destination: String,
-    val takeoff: Long
-)
+    val takeoff: Int,
+    val coordinates: Map<Int, Position>
+) {
+
+    fun getApproximatedPosition(timestamp: Int): Position {
+        val timestampOffset = timestamp - takeoff
+
+        if (coordinates.containsKey(timestampOffset)) return coordinates.getValue(timestampOffset)
+
+        var prevT = 0
+        var nextT = 0
+        for (key in coordinates.keys) {
+            if (key > timestampOffset) {
+                nextT = key
+                break
+            }
+            prevT = key
+        }
+        val prev = coordinates.getValue(prevT)
+        val next = coordinates.getValue(nextT)
+
+        val factor = (timestampOffset - prevT).toDouble() / (nextT - prevT)
+        return Position(
+            prev.lat + (next.lat - prev.lat) * factor,
+            prev.lon + (next.lon - prev.lon) * factor,
+            prev.alt + (next.alt - prev.alt) * factor
+        )
+    }
+}
 
 data class Position(
     val lat: Double,
@@ -46,12 +74,11 @@ data class Vector(val x: Double, val y: Double, val z: Double)
 
 data class AirportFlights(val start: String, val destination: String, val count: Int)
 
-const val radius = 6371_000
+data class Request(val flightId: Int, val timestamp: Int)
 
 val Double.rad: Double get() = PI * (this / 180)
-
 fun Position.toVector(): Vector {
-    val r = radius + alt
+    val r = 6371_000 + alt
     return Vector(
         cos(lat.rad) * cos(lon.rad) * r,
         cos(lat.rad) * sin(lon.rad) * r,
@@ -59,30 +86,35 @@ fun Position.toVector(): Vector {
     )
 }
 
+fun loadFlight(flightId: Int): Flight {
+    val scanner = scanner(File("input/usedFlights/${flightId}.csv").readText())
+
+    val origin = scanner.next()
+    val destination = scanner.next()
+    val takeoff = scanner.nextInt()
+
+    val count = scanner.nextInt()
+    val positions = (0 until count).map {
+        scanner.nextInt() to Position(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble())
+    }.toMap()
+
+    return Flight(origin, destination, takeoff, positions)
+}
+
 fun processFile(file: File): List<String> {
     println("processing ${file.name}...")
 
-    val text = file.readText()
-    val scanner = Scanner(text).apply {
-        useDelimiter(Pattern.compile("[\n,]"))
-        useLocale(Locale.ROOT)
-    }
+    val scanner = scanner(file.readText())
     val count = scanner.nextInt()
 
-    val positions = (0 until count).map {
-        Position(
-            scanner.nextDouble(),
-            scanner.nextDouble(),
-            scanner.nextDouble()
-        )
+    val request = (0 until count).map {
+        Request(scanner.nextInt(), scanner.nextInt())
     }
 
-    val vectors = positions.map {
-        it.toVector()
-    }
-
-    return vectors.map {
-        "${it.x} ${it.y} ${it.z}"
+    return request.map {
+        val flight = loadFlight(it.flightId)
+        val position = flight.getApproximatedPosition(it.timestamp)
+        return@map "${position.lat} ${position.lon} ${position.alt}"
     }
 
     /*
