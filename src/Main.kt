59095e8172aafaa612ed204042bfa04c88fa450a
Bernhard Danecker <bernhard.danecker@gmail.com>
2020-04-03 16:11:08 +0200

    level3 solution

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,13 @@
 import java.io.File
 import java.util.*
 import java.util.regex.Pattern
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 fun main() {
 
-    val level = "level2"
+    val level = "level3"
 
     File("input/$level").listFiles { _, filename -> filename.endsWith(".in") }!!.sortedBy { it.name }.forEach {
         val result = processFile(it)
@@ -33,18 +36,28 @@ data class Flight(
     val takeoff: Long
 )
 
-data class MinMax(
-    val minTimestamp: Long,
-    val maxTimestamp: Long,
-    val minLat: Float,
-    val maxLat: Float,
-    val minLon: Float,
-    val maxLon: Float,
-    val minAlt: Float,
-    val maxAlt: Float
+data class Position(
+    val lat: Double,
+    val lon: Double,
+    val alt: Double
 )
 
+data class Vector(val x: Double, val y: Double, val z: Double)
+
 data class AirportFlights(val start: String, val destination: String, val count: Int)
+
+const val radius = 6371_000
+
+val Double.rad: Double get() = PI * (this / 180)
+
+fun Position.toVector(): Vector {
+    val r = radius + alt
+    return Vector(
+        cos(lat.rad) * cos(lon.rad) * r,
+        cos(lat.rad) * sin(lon.rad) * r,
+        sin(lat.rad) * r
+    )
+}
 
 fun processFile(file: File): List<String> {
     println("processing ${file.name}...")
@@ -56,6 +69,23 @@ fun processFile(file: File): List<String> {
     }
     val count = scanner.nextInt()
 
+    val positions = (0 until count).map {
+        Position(
+            scanner.nextDouble(),
+            scanner.nextDouble(),
+            scanner.nextDouble()
+        )
+    }
+
+    val vectors = positions.map {
+        it.toVector()
+    }
+
+    return vectors.map {
+        "${it.x} ${it.y} ${it.z}"
+    }
+
+    /*
     val flights = (0 until count).map {
         Flight(
             scanner.nextLong(),
@@ -78,4 +108,5 @@ fun processFile(file: File): List<String> {
     return groups.map {
         "${it.start} ${it.destination} ${it.count}"
     }
+    */
 }
